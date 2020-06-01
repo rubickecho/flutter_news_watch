846a2fca77a717379edef40fdae1bb9637a31c48
peakcool <tangliangcheng852@gmail.com>
2020-06-01 21:26:22 +0800

    test web

---
+++ android/app/src/main/kotlin/peakcool/flutter/flutter_news_watch/MainActivity.kt
@@ -0,0 +1,13 @@
+package peakcool.flutter.flutter_news_watch
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
